4cb07adeeb96db2b7805aca815f48d19a86ead52
Phil Paolo <philipcpaolo@gmail.com>
2020-08-25 18:23:59 -0400

    create expense and income function constructors

--- src/app.js
+++ src/app.js
@@ -6,7 +6,29 @@
 // BUDGET CONTROLLER
 let budgetController = (function() {
 
+    let Expense = function(id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
 
+    let Income = function(id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };    
+
+    let data = {
+       allItems: {
+           exp: [],
+           inc: []
+       },
+       total: {
+           exp: 0,
+           inc: 0
+       }
+    }
+    
 })();
 
 
