95e93642dd958a44955e54c674347b44499f7da5
Phil Paolo <philipcpaolo@gmail.com>
2020-08-26 07:47:53 -0400

    add missing comma to data structure to populate object

--- src/app.js
+++ src/app.js
@@ -5,57 +5,56 @@
 
 // BUDGET CONTROLLER
 let budgetController = (function() {
-
+    
     let Expense = function(id, description, value) {
         this.id = id;
         this.description = description;
         this.value = value;
     };
-
+    
     let Income = function(id, description, value) {
         this.id = id;
         this.description = description;
         this.value = value;
-    };    
-
-    let data = {
-       allItems: {
-           exp: [],
-           inc: []
-       },
-       totals: {
-           exp: 0,
-           inc: 0
-       }
     };
-
+    
+    let data = {
+        allItems: {
+            exp: [],
+            inc: []
+        },
+        totals: {
+            exp: 0,
+            inc: 0
+        },
+    };
+    
     return {
         addItem: function(type, des, val) {
             let newItem, ID;
-            // Create new ID            
+            // Create new ID
             if(data.allItems[type].length > 0) {
                 ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
             } else {
                 ID = 0;
-            }                  
+            }
             // Create new item based on type
             if(type === 'inc') {
                 newItem = new Income(ID, des, val);
-                console.log(type + ' this is the type')
             } else if(type === 'exp') {
                 newItem = new Expense(ID, des, val);
             }
             // Push it into our data structure
-            data.allItems[type].push(newItem);
+            data.allItems[type].push(newItem);           
             // Return the new element
-            console.log(newItem + ' this is the new item');
             return newItem;
-        },
-
+        },       
+        
         testing: function() {
-            console.log('this is some data ' + data);
+            console.log(data);
         }
     };
+    
 })();
 
 
@@ -63,68 +62,70 @@ let budgetController = (function() {
 
 // UI CONTROLLER
 let UIController = (function() {
-
-    let DOMStrings = {
+    
+    let DOMstrings = {
         inputType: '.add__type',
         inputDescription: '.add__description',
         inputValue: '.add__value',
-        inputBtn: '.add__btn'
+        inputBtn: '.add__btn',
     };
-
+    
     return {
         getInput: function() {
             return {
-                type: document.querySelector(DOMStrings.inputType).value, // will be inc or exp
-                description: document.querySelector(DOMStrings.inputDescription).value,
-                value: document.querySelector(DOMStrings.inputValue).value
+                type: document.querySelector(DOMstrings.inputType).value, // Will be either inc or exp
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: parseFloat(document.querySelector(DOMstrings.inputValue).value)
             };
         },
-
+        
         getDOMstrings: function() {
-            return DOMStrings;
+            return DOMstrings;
         }
     };
-
+    
 })();
 
 
 
 // GLOBAL APP CONTROLLER
 let appController = (function(budgetCtrl, UICtrl) {
-
+    
     let setupEventListeners = function() {
         let DOM = UICtrl.getDOMstrings();
-
+        
         document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
         document.addEventListener('keypress', function(event) {
-            if(event.keyCode === 13) {
+            if (event.keyCode === 13) {
                 ctrlAddItem();
             }
-        });
+        });       
     };
-
-
-
+    
+    
+    
     let ctrlAddItem = function() {
-        let newItem, input;
-        // 1. get the field input data
-        input = UICtrl.getInput();
-        // 2. add the item to the budget controller
-        newItem = budgetCtrl.addItem(input.type, input.description, input.value); 
-
+        let input, newItem;
+        
+        // 1. Get the field input data
+        input = UICtrl.getInput();        
+        
+        // 2. Add the item to the budget controller
+        newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+        
         // 3. Add the item to the UI
         // 4. calculate the budget
         // 5. display the budget on the UI
     };
-
+    
     return {
         init: function() {
-            console.log('App has started.');
+            console.log('Application has started.');
             setupEventListeners();
         }
     };
-
+    
 })(budgetController, UIController);
 
 
